f960a13ba5b7fa63fe247ce17c421aedacaeca8d
333Nikita333 <nikitosfox333@gmail.com>
2023-03-08 23:50:47 +0200

    added closing the form modal window on the ESCAPE key and clicking on the backdrop, fixed mobile navigation menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,43 +1,53 @@
 (() => {
-    const menuBtnRef = document.querySelector("[data-menu-button]");
-    const mobileMenuRef = document.querySelector("[data-menu]");
-  
-    menuBtnRef.addEventListener("click", () => {
-      const expanded =
-        menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-  
-      menuBtnRef.classList.toggle("button-menu--is-open");
-      menuBtnRef.setAttribute("aria-expanded", !expanded);
-  
-      mobileMenuRef.classList.toggle("mobile-menu--is-open");
-    });
-  })()
-  
-  // (() => {
-  //   const mobileMenu = document.querySelector('.js-menu-container');
-  //   const openMenuBtn = document.querySelector('.js-open-menu');
-  //   const closeMenuBtn = document.querySelector('.js-close-menu');
-  
-  //   const toggleMenu = () => {
-  //     const isMenuOpen =
-  //       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-  //     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-  //     mobileMenu.classList.toggle('is-open');
-  
-  //     const scrollLockMethod = !isMenuOpen
-        // ? 'disableBodyScroll'
-  //       : 'enableBodyScroll';
-  //     bodyScrollLock[scrollLockMethod](document.body);
-  //   };
-  
-  //   openMenuBtn.addEventListener('click', toggleMenu);
-  //   closeMenuBtn.addEventListener('click', toggleMenu);
-  
-  //   // Close the mobile menu on wider screens if the device orientation changes
-  //   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-  //     if (!e.matches) return;
-  //     mobileMenu.classList.remove('is-open');
-  //     openMenuBtn.setAttribute('aria-expanded', false);
-  //     bodyScrollLock.enableBodyScroll(document.body);
-  //   });
-  // })();
+  const menuBtnRefOpen = document.querySelector('[data-menu-button-open]');
+  const menuBtnRefClose = document.querySelector('[data-menu-button-close]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
+  const body = document.querySelector('.body-site');
+
+  menuBtnRefOpen.addEventListener('click', () => {
+    menuBtnRefOpen.classList.toggle('button-menu--is-open');
+
+    mobileMenuRef.classList.toggle('mobile-menu--is-open');
+  });
+
+  menuBtnRefClose.addEventListener('click', () => {
+    menuBtnRefClose.classList.toggle('button-menu--is-close');
+
+    body.classList.toggle('menu--is-open');
+
+    mobileMenuRef.classList.toggle('mobile-menu--is-open');
+  });
+
+  menuBtnRefOpen.addEventListener('click', () => {
+    body.classList.toggle('menu--is-open');
+  });
+})();
+
+// (() => {
+//   const mobileMenu = document.querySelector('.js-menu-container');
+//   const openMenuBtn = document.querySelector('.js-open-menu');
+//   const closeMenuBtn = document.querySelector('.js-close-menu');
+
+//   const toggleMenu = () => {
+//     const isMenuOpen =
+//       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+//     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+//     mobileMenu.classList.toggle('is-open');
+
+//     const scrollLockMethod = !isMenuOpen
+// ? 'disableBodyScroll'
+//       : 'enableBodyScroll';
+//     bodyScrollLock[scrollLockMethod](document.body);
+//   };
+
+//   openMenuBtn.addEventListener('click', toggleMenu);
+//   closeMenuBtn.addEventListener('click', toggleMenu);
+
+//   // Close the mobile menu on wider screens if the device orientation changes
+//   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+//     if (!e.matches) return;
+//     mobileMenu.classList.remove('is-open');
+//     openMenuBtn.setAttribute('aria-expanded', false);
+//     bodyScrollLock.enableBodyScroll(document.body);
+//   });
+// })();
--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,43 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    body: document.querySelector('.body-site'),
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', openModal);
+  refs.closeModalBtn.addEventListener('click', closeModal);
+  refs.modal.addEventListener('click', handleBackdropClick);
+  
+  function openModal() {
+    refs.modal.classList.toggle('backdrop--is-hidden');
+    refs.body.classList.toggle('menu--is-open');
 
-  function toggleModal() {
-    refs.modal.classList.toggle("backdrop--is-hidden");
+    window.addEventListener('keydown', handleEscDown);
+  }
+  function closeModal() {
+    refs.modal.classList.toggle('backdrop--is-hidden');
+    refs.body.classList.toggle('menu--is-open');
+    window.removeEventListener('keydown', handleEscDown);
+  }
+  function handleBackdropClick(e) {
+    if (refs.modal.classList.contains('backdrop--is-hidden')) {
+      return;
+    }
+  
+    if (e.currentTarget === e.target) {
+      refs.modal.classList.toggle('backdrop--is-hidden');
+    }
+  }
+  function handleEscDown(e) {
+    if (refs.modal.classList.contains('backdrop--is-hidden')) {
+      return;
+    }
+
+    if (e.code === 'Escape') {
+      refs.modal.classList.toggle('backdrop--is-hidden');
+      refs.body.classList.toggle('menu--is-open');
+    }
   }
 })();
